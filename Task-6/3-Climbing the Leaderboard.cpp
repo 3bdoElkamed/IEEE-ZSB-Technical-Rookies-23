414bf289df7ba30cae300910d4c1fa146fea9374
Abdulrahman Ibrahim <43120443+3bdoElkamed@users.noreply.github.com>
2022-12-10 23:21:52 +0200

    Task-6

---
+++ Task-6/3-Climbing the Leaderboard.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define itn int
+#define ni int n;cin>>n; //input n
+#define nmi int n,m;cin>>n>>m; //input n,m
+#define forn for(int i=0;i<n;i++) //1D Array 
+#define forn1 for(int i=1;i<=n;i++) //for n start 1
+#define si string s;cin>>s; //input string s
+#define gsi string s;getline(cin,s); //input getline string
+#define forsi for(int i=0;i<s.size();i++) //string
+#define for2n for(int i=0;i<n;i++)for(int j=0;j<n;j++) //2D array n*n  or searching
+#define fornm for(int i=0;i<n;i++)for(int j=0;j<m;j++) //2D array n*m
+#define ina int a[n];for(int i=0;i<n;i++){cin>>a[i];} //input 1D array 
+#define outa for(int i=0;i<n;i++)cout<<a[i]<<" "; cout<<endl;//output 1D array //input 2D
+#define in2da int a[n][m];for(int i=0;i<n;i++){for(int j=0;j<m;j++)cin>>a[i][j];} //input 2D
+#define out2da for(int i=0;i<n;i++){for(int j=0;j<m;j++)cout<<a[i][j]<<" ";cout<<endl;} //output 2D
+#define all(s) s.begin(),s.end()
+#define fix(n) cout << fixed << setprecision(n);
+#define tt int t=0;cin>>t; while(t--) //test cases
+#define IO ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+#define no cout<<"NO"<<endl;
+#define yes cout<<"YES"<<endl;
+#define endl "\n"
+#define pi 3.14159265
+using namespace std;
+int main()
+{
+  IO
+  // #ifndef ONLINE_JUDGE
+  // freopen("input.txt", "r", stdin);
+  // freopen("output.txt", "w", stdout);
+  // #else
+  // #endif
+//////////////////////////////////////////
+  ni
+  ina
+  int r[n];
+  int m;cin>>m;
+  int b[m];
+  int j=1;
+  for(int i=0;i<m;i++)cin>>b[i];
+  // implement rank array
+  r[0]=j; //set first 1
+  for(int i=1;i<n;i++){
+    if(a[i]==a[i-1])j=j;
+    else j++;
+    r[i]=j;
+  }
+  int z=0;
+  while(m--){
+    if(b[z] >=a[0]){   // if the score is hightest
+      cout<<r[0]<<endl;
+      z++;
+    }else if(b[z]>a[n-1]){
+      n--;
+      m++;
+    }else if(b[z]==a[n-1]){  // check from last
+      cout<<r[n-1]<<endl;
+      z++;
+    }else{
+      cout<<r[n-1]+1<<endl;
+      z++;
+    }
+  }
+
+  return 0;
+}
